858c9c591809f25f39f27682484859d60009b866
unknown <sylvanusanang45@gmail.com>
2026-09-04 12:11:20 +0000

    Fix import capitalization

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 import NoteForm from "./NoteForm";
-import SearchBar from "./SearchBgitar";
+import SearchBar from "./SearchBar";
 import FilterButtons from "./FilterButtons";
 import NoteItem from "./NoteItem";
 
